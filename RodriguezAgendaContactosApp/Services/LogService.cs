1363f6552439cf42eca5fbe253ee851a4810a4c6
Jeanpro2004 <jean.rodriguez.ortiz@udla.edu.ec>
2025-07-10 08:07:15 -0500

    Creacion de Servicio y View Model para los logs

---
+++ RodriguezAgendaContactosApp/Services/LogService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodriguezAgendaContactosApp.Services
+{
+    public static class LogService
+    {
+        private static string FilePath => Path.Combine(FileSystem.AppDataDirectory, "Logs_Rodriguez.txt");
+
+        public static async Task AppendLogAsync(string nombre)
+
+        {
+            string log = $"Se incluyo el registro {nombre} el {DateTime.Now:dd/MM/yyyy HH:mm}\n";
+            await File.AppendAllTextAsync(FilePath, log);
+        }
+
+        public static async Task<string> ReadLogsAsync()
+        {
+            if (!File.Exists(FilePath))
+            return await File.ReadAllTextAsync(FilePath);
+            return "No existen aun logs";
+        }
+    }
+}
---
+++ RodriguezAgendaContactosApp/ViewModels/LogsViewModel.cs
@@ -0,0 +1,27 @@
+﻿using AVFoundation;
+using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RodriguezAgendaContactosApp.ViewModels
+{
+    public partial class LogsViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string contenidoLogs;
+
+        public LogsViewModel()
+        {
+            CargarLogs();
+        }
+
+        private async void CargarLogs()
+        {
+            contenidoLogs = await LogService.ReadLogsAsync();
+        }
+
+    }
+}
