94cb160a0b0b8ea9554f71c76e92b79c0950d77b
laboratorio <laboratorio@UPOAULA10714>
2025-07-10 07:16:57 -0500

    Agregar archivos de proyecto.

---
+++ RodriguezAgendaContactosApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RodriguezAgendaContactosApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ RodriguezAgendaContactosApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RodriguezAgendaContactosApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
