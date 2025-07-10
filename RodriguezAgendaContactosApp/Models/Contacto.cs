ed93af23de3464febd8b9e17dfab3c5922efc3b4
Jeanpro2004 <jean.rodriguez.ortiz@udla.edu.ec>
2025-07-10 07:25:32 -0500

    Implementacion de la carpeta modelos y modelo principal Contacto para el funcionamiento del sistema

---
+++ RodriguezAgendaContactosApp/Models/Contacto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace RodriguezAgendaContactosApp.Models
+{
+    class Contacto
+    {
+        [Primary Key, AutoIncrement]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+
+        public bool Favorito { get; set; }
+
+
+    }
+}
